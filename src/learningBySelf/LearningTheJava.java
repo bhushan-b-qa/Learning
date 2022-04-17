eb8405e21b25355b920e65f0ecc3da42ed3c9cbe
Bhushan <bhushan_b@outlook.com>
2022-04-17 16:09:53 +0530

    Second commit

--- src/learningBySelf/LearningTheJava.java
+++ src/learningBySelf/LearningTheJava.java
@@ -3,5 +3,6 @@ package learningBySelf;
 public class LearningTheJava {
     public static void main(String[] args){
         System.out.println("Hello World");
+        System.out.println("Second commit for Git repo");
     }
 }
